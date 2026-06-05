d0fa8ae827e7be303d8ffdb4a9cf8b5d76803205
AryanDhane <dhanearyan150074@gmail.com>
2026-06-05 17:38:59 +0530

    ..

--- single element in sorted array/ex1.cpp
+++ single element in sorted array/ex1.cpp
@@ -2,6 +2,37 @@
 #include <vector>
 using namespace std;
 
-int singleelement(vector<int>& nums) {
-             
+ int singleNonDuplicate(vector<int>& nums) {
+
     
+        int end = 0;
+        int st = nums.size() - 1;
+
+        while(end < st) {
+
+            int mid = end + (st - end) / 2;
+
+            if(mid % 2 == 1)
+                mid--;
+
+            if(nums[mid] == nums[mid + 1]) {
+                end = mid + 2;
+            }
+            else {
+                st = mid;
+            }
+        }
+
+        return nums[end];
+    }
+    
+    int main() {
+        vector<int> nums = {1,1,2,3,3,4,4,8,8};
+        cout << singleNonDuplicate(nums) << endl;
+        return 0;
+    }
+    
+
+
+
+// output: 2 
