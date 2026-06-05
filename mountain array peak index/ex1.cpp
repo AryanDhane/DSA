0ea195439f09174b6f8cfbd0391ea930fe34c7f9
AryanDhane <dhanearyan150074@gmail.com>
2026-06-05 17:36:46 +0530

    ..

--- mountain array peak index/ex1.cpp
+++ mountain array peak index/ex1.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int peakIndexInMountainArray(vector<int>& arr) {
-    int st=0, end=arr.size()-1;
+    int st=0, end=arr.size()-1;                 
 
     while(st<end){
         int mid=st+(end-st)/2;
 
         if(arr[mid] < arr[mid+1]){
             st=mid+1;
-        }
+        } 
         else{
             end=mid;
         }
@@ -25,4 +25,4 @@ int main() {
     vector<int> vec(arr, arr + sizeof(arr) / sizeof(arr[0]));
     cout << peakIndexInMountainArray(vec) << endl;
     return 0;
-}
+}    
