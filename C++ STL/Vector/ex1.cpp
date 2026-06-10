f6cf067795ebcb32f0f05ab80b6b1c1756d8a97b
AryanDhane <dhanearyan150074@gmail.com>
2026-06-10 08:34:07 +0530

    ..

--- C++ STL/Vector/ex1.cpp
+++ C++ STL/Vector/ex1.cpp
@@ -7,6 +7,14 @@ int main() {
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    v.pop_back();
+    for (int val : v) {
+        cout << val << " ";
+    }
+
+    v.erase(v.begin() + 1);
 
     cout << "Elements in the vector: ";
     
@@ -16,6 +24,6 @@ int main() {
     cout << endl;
     cout << "Size:"<< v.size() << endl;
     cout << endl;
-
+    
     return 0;
 }
