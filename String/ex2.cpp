fe94e68c7c3fa76d7f6f1923d8b43bf653c1b16c
AryanDhane <dhanearyan150074@gmail.com>
2026-06-12 15:39:59 +0530

    ..

---
+++ String/ex2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void reverseString(vector<char>& s) {
+      reverse(s.begin(), s.end());
+    }
+
+int main() {
+
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+    reverseString(s);
+
+    cout << "Reversed string: ";
+    for (char c : s) {
+        cout << c;
+    }
+    cout << endl;
+
+    return 0;
+} 
