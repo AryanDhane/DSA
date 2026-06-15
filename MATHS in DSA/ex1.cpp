3a95fac7209c2b8341a98fee6dda59e98049d076
AryanDhane <dhanearyan150074@gmail.com>
2026-06-15 10:07:26 +0530

    ..

--- MATHS in DSA/ex1.cpp
+++ MATHS in DSA/ex1.cpp
@@ -2,9 +2,18 @@
 #include <vector>
 using namespace std;
 
+bool isprime(int n){
+    for(int i=2; i*i<=n; i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(){
-
-
-    
+    int n = 4;
+    cout << isprime(n) << endl ;
+     
     return 0;
 }
