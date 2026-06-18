7b558670297effd70030d675b7ce33c8142bb2b0
AryanDhane <dhanearyan150074@gmail.com>
2026-06-18 07:29:07 +0530

    ..

---
+++ Recursion/ex1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printNum(int n) {
+    if( n == 1){
+        cout << "1\n";
+    }
+    cout << n << " ";
+    printNum(n-1);
+}
+
+int main () {
+    printNum(100);
+    return 0;
+}
